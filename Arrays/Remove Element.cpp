3f2fd17b2d5aea08059d2c3740fabd5f9bbd255e
Altaf Hussain <altafhussain1808@gmail.com>
2022-10-11 23:34:55 +0530

    commit

---
+++ Arrays/Remove Element.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if(nums.size()<0)
+            return 0;
+        int res=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=val)
+            {
+                 nums[res]=nums[i];
+                 res++;
+            }
+        }
+        return res;
+    }
+};
